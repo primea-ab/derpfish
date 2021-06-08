4c0363748c19a680ddfd2deeab3a5f5d834d0559
Tomas Hasselquist <tomas@hasselquist.se>
2021-06-08 22:59:00 +0200

    Add almost functioning fetch of allowed moves for linear movement

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gookit/color"
+	"os"
 	"strings"
 	"unicode"
 )
@@ -20,16 +22,100 @@ const BLACK = 16
 func main() {
 	fmt.Println("Starting Derpfish")
 	board := createNewBoard()
-	displayBoard(WHITE, board)
+	board = createBoardFromFen("r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1") // REPLACE STARTER BOARD FOR TESTING
+	startGame(board)
+}
+
+func startGame(board *[64]int) {
+	reader := bufio.NewReader(os.Stdin)
+	currentPlayer := WHITE
+	for {
+		displayBoard(currentPlayer, board, []int{})
+		fromSquare := getCommand("From ", reader)
+		allowedMoves := getAllowedMoves(currentPlayer, board, fromSquare)
+		fmt.Println(allowedMoves)
+		displayBoard(currentPlayer, board, allowedMoves)
+		toSquare := getCommand(fromSquare + " -> ? ", reader)
+		fmt.Println(fromSquare, toSquare)
+	}
+}
+
+func getCommand(inputtext string, reader *bufio.Reader) string {
+	fmt.Print(inputtext)
+	cmd, _ := reader.ReadString('\n')
+	return strings.Replace(cmd, "\n", "", -1)
 }
 
 func createNewBoard() *[64]int {
 	return createBoardFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
 }
 
+func getAllowedMoves(currentPlayer int, board *[64]int, fromSquare string) []int {
+	position := []rune(fromSquare)
+	file := position[0]-97
+	rank := position[1]-49
+	square := int(rank * 8 + file)
+
+	uncoloredPiece := board[square] &^ currentPlayer
+	if uncoloredPiece > 8 {
+		return []int{}
+	}
+
+	return getMovementForPiece(board, square, uncoloredPiece)
+}
+
+func getMovementForPiece(board *[64]int, square int, piece int) []int {
+	switch piece {
+	case PAWN:
+		return getPawnMovement(board, square)
+	case KING:
+		return getKingMovement(board, square)
+	case KNIGHT:
+		return getKnightMovement(board, square)
+	case BISHOP:
+		fmt.Println("I AM BISH")
+		return getPossibleLinearMovement(board, square, []int{7, 9, -7, -9})
+	case ROOK:
+		return getPossibleLinearMovement(board, square, []int{1, 8, -1, -8})
+	case QUEEN:
+		return getPossibleLinearMovement(board, square, []int{7, 9, -7, -9, 1, 8, -1, -8})
+	default:
+		return []int{}
+	}
+}
+
+func getPawnMovement(board *[64]int, square int) []int {
+	return []int{}
+}
+
+func getKingMovement(board *[64]int, square int) []int {
+	return []int{}
+}
+
+func getKnightMovement(board *[64]int, square int) []int {
+	return []int{}
+}
+
+func getPossibleLinearMovement(board *[64]int, square int, directions []int) []int {
+	var possibleMoves []int
+	for _, d := range directions {
+		checkedSquare := square + d
+		for {
+			fmt.Println("SQUARE")
+			fmt.Println(checkedSquare)
+			if (checkedSquare) < 0 || (checkedSquare) >= 64 || board[checkedSquare] != NONE {
+				break
+			}
+			possibleMoves = append(possibleMoves, checkedSquare)
+			checkedSquare += d
+		}
+	}
+	return possibleMoves
+}
+
 func createBoardFromFen(fen string) *[64]int {
 	board := [64]int{}
-	pieceFromSymbol := map[string]int{
+	pieceFromSymbol := map[string]int {
 		"k": BLACK | KING, "p": BLACK | PAWN, "n": BLACK | KNIGHT, "b": BLACK | BISHOP, "r": BLACK | ROOK, "q": BLACK | QUEEN,
 		"K": WHITE | KING, "P": WHITE | PAWN, "N": WHITE | KNIGHT, "B": WHITE | BISHOP, "R": WHITE | ROOK, "Q": WHITE | QUEEN,
 	}
@@ -53,6 +139,49 @@ func createBoardFromFen(fen string) *[64]int {
 	return &board
 }
 
+func displayBoard(side int, board *[64]int, possibleMovement []int) {
+	if side == WHITE {
+		for r := 7; r >= 0; r-- {
+			fmt.Print(r+1)
+			for f := 0; f < 8; f++ {
+				printSquare(board, side, r, f, has(possibleMovement, r*8+f))
+			}
+			fmt.Println()
+		}
+		fmt.Println("  a  b  c  d  e  f  g  h")
+	} else if side == BLACK {
+		for r := 0; r < 8; r++ {
+			fmt.Print(r+1)
+			for f := 7; f >= 0; f-- {
+				printSquare(board, side, r, f, has(possibleMovement, r*8+f))
+			}
+			fmt.Println()
+		}
+		fmt.Println("  h  g  f  e  d  c  b  a")
+	}
+}
+
+func has(slice []int, val int) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
+
+func printSquare(board *[64]int, side int, rank int, file int, canMove bool) {
+	fg := color.FgBlack
+	if (board[rank*8+file]) < BLACK {
+		fg = color.FgWhite
+	}
+	if canMove {
+		color.New(fg, color.BgRed).Printf(" %c ", getUnicodePrintOfSquare(board[rank*8+file]))
+	} else {
+		color.New(fg, getBackgroundColor(side, rank, file)).Printf(" %c ", getUnicodePrintOfSquare(board[rank*8+file]))
+	}
+}
+
 func getBackgroundColor(side int, rank int, file int) color.Color {
 	bgColor := color.BgWhite
 	if (side == WHITE && file % 2 == rank % 2) || (side == BLACK && file % 2 != rank % 2) {
@@ -61,29 +190,12 @@ func getBackgroundColor(side int, rank int, file int) color.Color {
 	return bgColor
 }
 
-func displayBoard(side int, board *[64]int) {
-	if side == WHITE {
-		for r := 7; r >= 0; r-- {
-			for f := 0; f < 8; f++ {
-				color.New(color.FgBlack, getBackgroundColor(side, r, f)).Printf(" %c ", getUnicodePrintOfSquare(board[r*8+f]))
-			}
-			fmt.Println()
-		}
-	} else if side == BLACK {
-		for r := 0; r < 8; r++ {
-			for f := 7; f >= 0; f-- {
-				color.New(color.FgBlack, getBackgroundColor(side, r, f)).Printf(" %c ", getUnicodePrintOfSquare(board[r*8+f]))
-			}
-			fmt.Println()
-		}
-	}
-}
-
+// TODO: Might simplify iconFromPiece later
 func getUnicodePrintOfSquare(square int) rune {
 	iconFromPiece := map[int]rune{
 		NONE: ' ',
 		BLACK | KING: '\u265A', BLACK | PAWN: '\u265F', BLACK | KNIGHT: '\u265E', BLACK | BISHOP: '\u265D', BLACK | ROOK: '\u265C', BLACK | QUEEN: '\u265B',
-		WHITE | KING: '\u2654', WHITE | PAWN: '\u2659', WHITE | KNIGHT: '\u2658', WHITE | BISHOP: '\u2657', WHITE | ROOK: '\u2656', WHITE | QUEEN: '\u2655',
+		WHITE | KING: '\u265A', WHITE | PAWN: '\u265F', WHITE | KNIGHT: '\u265E', WHITE | BISHOP: '\u265D', WHITE | ROOK: '\u265C', WHITE | QUEEN: '\u265B',
 	}
 	return iconFromPiece[square]
 }
