f13cc5a50cf186b47f9afd2b293193cdb68e9447
Tomas Hasselquist <tomas@hasselquist.se>
2021-06-28 23:53:42 +0200

    Implement castling for allowed movments

--- cmd/main.go
+++ cmd/main.go
@@ -19,20 +19,37 @@ const QUEEN = 6
 const WHITE = 8
 const BLACK = 16
 
+type Meta struct {
+	currentPlayer  int
+	whiteQueenSide bool
+	whiteKingSide bool
+	blackQueenSide bool
+	blackKingSide bool
+	enPassant int
+}
+
 func main() {
 	fmt.Println("Starting Derpfish")
 	board := createNewBoard()
-	board = createBoardFromFen("QKb1k1nr/p2p2Np/n2B4/1p1NPp2/p1p5/2PP1Q2/P1P1K2p/q5R1") // REPLACE STARTER BOARD FOR TESTING
-	startGame(board)
+	meta := Meta{
+		currentPlayer: WHITE,
+		whiteQueenSide: true,
+		whiteKingSide: true,
+		blackQueenSide: true,
+		blackKingSide: true,
+		enPassant: -1,
+	}
+	board = createBoardFromFen("r3k2r/p2p2Np/n2B4/1p1NPp2/p1p5/2PP1Q2/P1P1K2p/R3K2R") // REPLACE STARTER BOARD FOR TESTING
+	startGame(board, &meta)
 }
 
-func startGame(board *[64]int) {
+func startGame(board *[64]int, meta *Meta) {
 	reader := bufio.NewReader(os.Stdin)
-	currentPlayer := WHITE
+	currentPlayer := meta.currentPlayer
 	for {
 		displayBoard(currentPlayer, board, []int{})
 		fromSquare := getCommand("From ", reader)
-		allowedMoves := getAllowedMoves(currentPlayer, board, fromSquare, 37)
+		allowedMoves := getAllowedMoves(board, fromSquare, meta)
 		fmt.Println(allowedMoves)
 		displayBoard(currentPlayer, board, allowedMoves)
 		toSquare := getCommand(fromSquare + " -> ? ", reader)
@@ -47,29 +64,36 @@ func getCommand(inputtext string, reader *bufio.Reader) string {
 }
 
 func createNewBoard() *[64]int {
-	return createBoardFromFen("Qnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+	return createBoardFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
 }
 
-func getAllowedMoves(currentPlayer int, board *[64]int, fromSquare string, enpassant int) []int {
+func getAllowedMoves(board *[64]int, fromSquare string, meta *Meta) []int {
 	position := []rune(fromSquare)
 	file := position[0]-97
 	rank := position[1]-49
 	square := int(rank * 8 + file)
 
-	uncoloredPiece := board[square] &^ currentPlayer
+	uncoloredPiece := board[square] &^ meta.currentPlayer
 	if uncoloredPiece > 8 {
 		return []int{}
 	}
 
-	return getMovementForPiece(board, square, uncoloredPiece, currentPlayer, enpassant)
+	queenSide := meta.whiteQueenSide
+	kingSide := meta.whiteKingSide
+	if meta.currentPlayer == BLACK {
+		queenSide = meta.blackQueenSide
+		kingSide = meta.blackKingSide
+	}
+
+	return getMovementForPiece(board, square, uncoloredPiece, meta.currentPlayer, meta.enPassant, queenSide, kingSide)
 }
 
-func getMovementForPiece(board *[64]int, square int, piece int, side int, enpassant int) []int {
+func getMovementForPiece(board *[64]int, square int, piece int, side int, enpassant int, queenSide bool, kingSide bool) []int {
 	switch piece {
 	case PAWN:
 		return getPawnMovement(board, square, side, enpassant)
 	case KING:
-		return getKingMovement(board, square, side, false, false)
+		return getKingMovement(board, square, side, queenSide, kingSide)
 	case KNIGHT:
 		return getKnightMovement(board, square)
 	case BISHOP:
@@ -120,14 +144,13 @@ func getPawnMovement(board *[64]int, square int, side int, enpassant int) []int 
 }
 
 func isOpponentPiece(side int, piece int) bool {
-	return piece > 0 && piece & side == 0
+	return piece > 0 && isNotFriendly(side, piece)
 }
 
 func isNotFriendly(side int, piece int) bool {
 	return piece & side == 0
 }
 
-// TODO: Implement castling
 // TODO: Implement forbidding moves when ending up in check
 func getKingMovement(board *[64]int, square int, side int, queenSideCastle bool, kingSideCastle bool) []int {
 	var possibleMoves []int
@@ -146,9 +169,27 @@ func getKingMovement(board *[64]int, square int, side int, queenSideCastle bool,
 			possibleMoves = append(possibleMoves, checkedSquare)
 		}
 	}
+	if queenSideCastle && isLineClearForSteps(board, square, -1, 3) {
+		possibleMoves = append(possibleMoves, square - 3)
+	}
+
+	if kingSideCastle && isLineClearForSteps(board, square, 1, 2) {
+		possibleMoves = append(possibleMoves, square + 2)
+	}
 	return possibleMoves
 }
 
+func isLineClearForSteps(board *[64]int, square int, direction int, steps int) bool {
+	for i := 1; i <= steps; i++ {
+		fmt.Println(square + direction * i)
+		if board[square + direction * i] != NONE {
+			return false
+		}
+	}
+	return true
+}
+
+// TODO: Implement knight movement
 func getKnightMovement(board *[64]int, square int) []int {
 	return []int{}
 }
@@ -218,7 +259,7 @@ func displayBoard(side int, board *[64]int, possibleMovement []int) {
 		for r := 7; r >= 0; r-- {
 			fmt.Print(r+1)
 			for f := 0; f < 8; f++ {
-				printSquare(board, side, r, f, has(possibleMovement, r*8+f))
+				printSquare(board, r, f, has(possibleMovement, r*8+f))
 			}
 			fmt.Println()
 		}
@@ -227,7 +268,7 @@ func displayBoard(side int, board *[64]int, possibleMovement []int) {
 		for r := 0; r < 8; r++ {
 			fmt.Print(r+1)
 			for f := 7; f >= 0; f-- {
-				printSquare(board, side, r, f, has(possibleMovement, r*8+f))
+				printSquare(board, r, f, has(possibleMovement, r*8+f))
 			}
 			fmt.Println()
 		}
@@ -244,7 +285,7 @@ func has(slice []int, val int) bool {
 	return false
 }
 
-func printSquare(board *[64]int, side int, rank int, file int, canMove bool) {
+func printSquare(board *[64]int, rank int, file int, canMove bool) {
 	fg := color.FgBlack
 	if (board[rank*8+file]) < BLACK {
 		fg = color.FgWhite
@@ -252,13 +293,13 @@ func printSquare(board *[64]int, side int, rank int, file int, canMove bool) {
 	if canMove {
 		color.New(fg, color.BgRed).Printf(" %c ", getUnicodePrintOfSquare(board[rank*8+file]))
 	} else {
-		color.New(fg, getBackgroundColor(side, rank, file)).Printf(" %c ", getUnicodePrintOfSquare(board[rank*8+file]))
+		color.New(fg, getBackgroundColor(rank, file)).Printf(" %c ", getUnicodePrintOfSquare(board[rank*8+file]))
 	}
 }
 
-func getBackgroundColor(side int, rank int, file int) color.Color {
+func getBackgroundColor(rank int, file int) color.Color {
 	bgColor := color.BgWhite
-	if (side == WHITE && file % 2 == rank % 2) || (side == BLACK && file % 2 != rank % 2) {
+	if file % 2 == rank % 2 {
 		bgColor = color.BgGray
 	}
 	return bgColor
